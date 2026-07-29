6da3f0ac973b1710927f40ed517823a6db39b33b
Andrei Andreev <razenpok@gmail.com>
2026-07-29 22:37:05 +0200

    Cleanup the project

--- src/razen/microforge/TemplateModPlugin.java
+++ src/razen/microforge/TemplateModPlugin.java
@@ -1,4 +1,4 @@
-package author.modname;
+package razen.microforge;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
@@ -7,9 +7,6 @@ public class TemplateModPlugin extends BaseModPlugin {
     @Override
     public void onApplicationLoad() throws Exception {
         super.onApplicationLoad();
-
-        // Test that the .jar is loaded and working, using the most obnoxious way possible.
-        throw new RuntimeException("Template mod loaded and working!\nRemove this crash in TemplateModPlugin.");
     }
 
     @Override
